20fdaf88ea9468fb6668c97aa0e1219377572771
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-12 03:38:20 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/jira/issues/CreateCommentTest.java
+++ src/test/java/io/kestra/plugin/jira/issues/CreateCommentTest.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @KestraTest
-@WireMockTest(httpPort = 8081)
+@WireMockTest(httpPort = 5083)
 class CreateCommentTest {
 
     @Inject
