626895948a3b122df078066c2a1a7f6de302f059
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-12 03:36:06 +0530

    test: use WireMock for mocking HTTP calls in tests

--- src/test/java/io/kestra/plugin/jira/issues/CreateCommentTest.java
+++ src/test/java/io/kestra/plugin/jira/issues/CreateCommentTest.java
@@ -1,54 +1,48 @@
 package io.kestra.plugin.jira.issues;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Objects;
-import java.util.concurrent.TimeoutException;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+
 import io.kestra.core.junit.annotations.KestraTest;
-import io.kestra.core.models.executions.Execution;
-import io.kestra.core.queues.QueueException;
-import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.runners.TestRunner;
-import io.kestra.core.runners.TestRunnerUtils;
-import io.kestra.core.tenant.TenantService;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
 
 import jakarta.inject.Inject;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 
 @KestraTest
+@WireMockTest(httpPort = 8081)
 class CreateCommentTest {
 
     @Inject
-    protected TestRunner runner;
-
-    @Inject
-    protected TestRunnerUtils runnerUtils;
-
-    @Inject
-    protected LocalFlowRepositoryLoader repositoryLoader;
-
-    @BeforeEach
-    void init() throws IOException, URISyntaxException {
-        repositoryLoader.load(Objects.requireNonNull(CreateCommentTest.class.getClassLoader().getResource("flows")));
-        this.runner.run();
-    }
+    private RunContextFactory runContextFactory;
 
     @Test
-    void flow() throws TimeoutException, QueueException {
-        Execution execution = runnerUtils.runOne(
-            TenantService.MAIN_TENANT,
-            "io.kestra.tests",
-            "comment-jira",
-            null,
-            null
-        );
+    void run(WireMockRuntimeInfo wireMockRuntimeInfo) throws Exception {
+        stubFor(any(urlPathEqualTo("/rest/api/2/issue/TEST-123/comment"))
+            .willReturn(okJson("{\"id\":\"12345\",\"body\":\"Test comment\"}")));
 
-        assertThat(execution.getTaskRunList(), hasSize(1));
+        RunContext runContext = runContextFactory.of(Map.of());
+
+        CreateComment task = CreateComment.builder()
+            .baseUrl(wireMockRuntimeInfo.getHttpBaseUrl())
+            .username(Property.ofValue("test@test.com"))
+            .password(Property.ofValue("api-token"))
+            .projectKey(Property.ofValue("TEST"))
+            .issueIdOrKey("TEST-123")
+            .body(Property.ofValue("Test comment body"))
+            .build();
+
+        task.run(runContext);
+
+        verify(postRequestedFor(urlPathEqualTo("/rest/api/2/issue/TEST-123/comment"))
+            .withHeader("Content-Type", equalTo("application/json"))
+            .withHeader("Authorization", matching("Basic .+")));
     }
 }
